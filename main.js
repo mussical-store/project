28df15e6b2c6b9aa47ec272736e783663af7ac20
Nassir01 <you@example.com>
2020-12-18 17:06:18 +0100

    local storage working

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 $("#hide").hide()
 $("#logo").click(function(){
     $("#hide").toggle(3000)
+    $("#hide").hide(5000)
 })
-var myIndex = 0;
+    $("#shop").hide()
+    $("#bt").click(function(){
+      var instement = JSON.parse(localStorage.getItem('ins'))
+      $("#shop").empty()
+      for(var i = 0 ; i < instement.length ; i++){
+        $("#shop").append( '<li><button id="al"><h5 id="iso">' + instement[i] + '</h5></button></li>')
+       }
+
+      $("#shop").toggle()
+    })        
+var instement = [{
+  src : "https://www.noizikidz.com/12597-large_nzk3/guitare-electrique-type-s-noire.jpg" , 
+  name : "Guitar" 
+},
+{
+  src : "https://bentleymusic.com/wp-content/uploads/2020/07/TAMAIES1465-BOW-1-400x400.jpg"  , 
+  name : "drum" 
+},
+{
+  src : "https://www.cdiscount.com/pdt2/8/5/4/1/700x700/yam4957812593854/rw/clavier-61-touches-yamaha-np12b.jpg"  , 
+  name : "piano" 
+}]
+
+         for(var i = 0 ; i < instement.length ; i++){
+          $("#buttonimage").append('<img style="cursor: pointer;"onclick="addtochariot(' + 'instement[' + i +'].name'+')" src=' + instement[i].src + ' width="100" height="100">' + '<h5>' + instement[i].name + '</h5>')
+         }
+
+             var myIndex = 0;
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -15,6 +43,19 @@ function carousel() {
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 carousel();
+function check(){
+  localStorage.getItem("ins")
+  console.log(localStorage.getItem("ins"))
+}
+
+var ins = []
+function addtochariot(src){
+  ins.push(src)
+  localStorage.setItem("ins" , JSON.stringify(ins))
+console.log(src)
+}
+
+
 
 
 
